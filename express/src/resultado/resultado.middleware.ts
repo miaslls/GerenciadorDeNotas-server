2d38abc50170291cc969fcc4f557faba4fe6044c
miaslls <97785569+miaslls@users.noreply.github.com>
2023-12-29 18:58:05 -0300

    middleware validateBody

---
+++ express/src/resultado/resultado.middleware.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from 'express';
+import { BimestreLabel, DisciplinaLabel } from '@prisma/client';
+
+export function validateBody(req: Request, res: Response, next: NextFunction) {
+  const body = req.body;
+  const errorMessages = [];
+
+  if (!body.hasOwnProperty('bimestre') || body.bimestre === '') {
+    errorMessages.push("⚠️ O campo 'bimestre' não foi preenchido");
+  } else if (!BimestreLabel.hasOwnProperty(body.bimestre)) {
+    errorMessages.push(
+      "⚠️ O campo 'bimestre' deve ser: 'PRIMEIRO', 'SEGUNDO', 'TERCEIRO', ou 'QUARTO'"
+    );
+  }
+
+  if (!body.hasOwnProperty('disciplina') || body.disciplina === '') {
+    errorMessages.push("⚠️ O campo 'disciplina' não foi preenchido");
+  } else if (!DisciplinaLabel.hasOwnProperty(body.disciplina)) {
+    errorMessages.push(
+      "⚠️ O campo 'disciplina' deve ser: 'Biologia', 'Artes', 'Geografia', ou 'Sociologia'"
+    );
+  }
+
+  if (!body.hasOwnProperty('nota') || body.nota === '') {
+    errorMessages.push("⚠️ O campo 'nota' não foi preenchido");
+  } else if (isNaN(body.nota) || body.nota < 0 || body.nota > 10) {
+    errorMessages.push(
+      "⚠️ O campo 'nota' deve ser um número maior ou igual a 0, e menor ou igual a 10"
+    );
+  }
+
+  if (errorMessages.length > 0) {
+    const errorMessage =
+      'O Registro não foi criado: ' + errorMessages.join(' - ');
+
+    return res.status(400).send({ Erro: errorMessage });
+  }
+
+  next();
+}
--- express/src/resultado/resultado.router.ts
+++ express/src/resultado/resultado.router.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { create, getAll, remove } from './resultado.controller';
+import { validateBody } from './resultado.middleware';
 
 const resultadoRouter = express.Router();
 
 resultadoRouter.get('/', getAll);
-resultadoRouter.post('/', create);
+resultadoRouter.post('/', validateBody, create);
 resultadoRouter.delete('/:id', remove);
 
 export { resultadoRouter };
