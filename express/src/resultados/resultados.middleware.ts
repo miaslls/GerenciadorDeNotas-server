03052eb1ef4b504955f8ed95cbdeed6f80e78add
miaslls <97785569+miaslls@users.noreply.github.com>
2023-12-30 02:38:51 -0300

    refactor errorMessages

--- express/src/resultados/resultados.middleware.ts
+++ express/src/resultados/resultados.middleware.ts
@@ -1,45 +1,39 @@
 import { Request, Response, NextFunction } from 'express';
-import { BimestreLabel, DisciplinaLabel } from '@prisma/client';
+import { Bimestre, Disciplina } from '@prisma/client';
 import { validate as uuidValidate, version as uuidVersion } from 'uuid';
 
 export function validateBody(req: Request, res: Response, next: NextFunction) {
   const body = req.body;
-  const errorMessages = [];
+  const errorMessages: {
+    bimestre?: string;
+    disciplina?: string;
+    nota?: string;
+  } = {};
 
-  if (!body.hasOwnProperty('bimestre') || body.bimestre === '') {
-    errorMessages.push('⚠️ Bimestre não foi preenchido');
-  } else if (!BimestreLabel.hasOwnProperty(body.bimestre)) {
-    errorMessages.push(
-      `⚠️ Bimestre inválido. As opções válidas são ${Object.keys(
-        BimestreLabel
-      ).join(', ')}`
-    );
+  if (!body.hasOwnProperty('bimestre')) {
+    errorMessages.bimestre = 'Bimestre não foi preenchido';
+  } else if (!Bimestre.hasOwnProperty(body.bimestre)) {
+    errorMessages.bimestre = `Bimestre inválido; As opções válidas são ${Object.keys(
+      Bimestre
+    ).join(', ')}`;
   }
 
-  if (!body.hasOwnProperty('disciplina') || body.disciplina === '') {
-    errorMessages.push('⚠️ Disciplina não foi preenchido');
-  } else if (!DisciplinaLabel.hasOwnProperty(body.disciplina)) {
-    errorMessages.push(
-      `⚠️ Disciplina inválido. As opções válidas são ${Object.keys(
-        DisciplinaLabel
-      ).join(', ')}`
-    );
+  if (!body.hasOwnProperty('disciplina')) {
+    errorMessages.disciplina = 'Disciplina não foi preenchido';
+  } else if (!Disciplina.hasOwnProperty(body.disciplina)) {
+    errorMessages.disciplina = `Disciplina inválida; As opções válidas são ${Object.keys(
+      Disciplina
+    ).join(', ')}`;
   }
 
-  if (!body.hasOwnProperty('nota') || body.nota === '') {
-    errorMessages.push('⚠️ Nota não foi preenchido');
-  } else if (isNaN(body.nota) || body.nota < 0 || body.nota > 10) {
-    errorMessages.push(
-      '⚠️ Nota inválido. Deve ser um número maior ou igual a 0, e menor ou igual a 10'
-    );
+  if (!body.hasOwnProperty('nota')) {
+    errorMessages.nota = 'Nota não foi preenchido';
+  } else if (typeof body.nota !== 'number' || body.nota < 0 || body.nota > 10) {
+    errorMessages.nota = 'Nota inválida; Deve ser um número entre 0 e 10';
   }
 
-  if (errorMessages.length > 0) {
-    const errorMessage =
-      'Falha ao criar resultado. Verifique os dados e tente novamente: ' +
-      errorMessages.join(' - ');
-
-    return res.status(400).send({ Erro: errorMessage });
+  if (Object.keys(errorMessages).length > 0) {
+    return res.status(400).send({ Erro: errorMessages });
   }
 
   next();
@@ -52,7 +46,7 @@ export function validateId(req: Request, res: Response, next: NextFunction) {
   if (!isValid) {
     return res
       .status(400)
-      .send({ Erro: '⚠️ ID inválido. Utilize um UUID (v4) válido' });
+      .send({ Erro: 'ID inválido; Utilize um UUID v4 válido' });
   }
 
   next();
