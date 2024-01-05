5d53960c8497957162b9cf05cfa76ded98a03953
miaslls <97785569+miaslls@users.noreply.github.com>
2024-01-05 07:16:17 -0300

    horse, not zebra 🦓 & nothing good after 2am

--- src/bimestres/bimestres.router.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { validateBimestre } from './bimestres.middleware';
-import { getByBimestre } from './bimestres.controller';
-
-const bimestresRouter = express.Router();
-
-bimestresRouter.get('/:bimestre', validateBimestre, getByBimestre);
-
-export { bimestresRouter };
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import cors from 'cors';
 import 'dotenv/config';
 
 import { resultadosRouter } from './resultados/resultados.router';
-import { bimestresRouter } from './bimestres/bimestres.router';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/resultados', resultadosRouter);
-app.use('/bimestres', bimestresRouter);
 
 app.listen(port, () => {
   console.info(`Server running @ port ${port} - - - - - 🏃💨 `);
